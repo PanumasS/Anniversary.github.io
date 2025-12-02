75421bc0477023a3795c834350d30efcc324fd36
PaumasS <panumassuthi@gmail.com>
2025-12-03 02:03:29 +0700

    Initial commit - Anniversary Letter (build ready)

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="node" />
+
+declare module '@vitejs/plugin-react-swc';
+declare module 'vite';
+declare module '*.svg';
+declare module '*.mp3';
+declare module '*.ogg';
+declare module '*.css';
+declare module '*.scss';
+declare module '*.module.css';
+/// <reference types="node" />
+
+declare module '@radix-ui/react-accordion';
+declare module 'lucide-react';
+
